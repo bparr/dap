63f147b389620cfc219d68b8f8cf641fb2fd05b7
Ben Parr <bparr@bparr.com>
2017-06-19 20:16:13 -0400

    Add mostly empty boosted_tree_2014.py which will use http://scikit-learn.org/stable/modules/ensemble.html#regression on the 2014 data.

---
+++ boosted_tree_2014.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Parse and run boosted tree learner on 2014 data.
+"""
+
+import csv
+import os
+
+DATA_DIRECTORY = '2014'
+
+def main():
+  print('Hello World')
+
+if __name__ == '__main__':
+    main()
