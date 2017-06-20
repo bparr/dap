65b803c3861a18ba9e407c761082fed2fc1aedb9
Ben Parr <bparr@bparr.com>
2017-06-19 21:04:50 -0400

    More parse logic.

--- boosted_tree_2014.py
+++ boosted_tree_2014.py
@@ -8,18 +8,48 @@ import csv_utils
 import os
 
 DATA_PATH = '2014/2014_Pheotypic_Data_FileS2.csv'
+PERICARP_LABEL = 'Pericarp_pigmentation'
 
+
+# TODO keep?
 def pretty_label(label):
   label = label.replace('%', 'percent').replace('(', 'in ').replace(')', '')
   return label.replace(' ', '_')
 
 
-def main():
+# TODO replace with Imputer.
+def float_or_empty(s):
+  return None if s == '' else float(s)
+
+
+def parse_data():
   lines = csv_utils.read_csv(DATA_PATH, ignore_first_lines=2)
   labels = [pretty_label(x) for x in lines[0]]
-  print('\n'.join(labels))
   samples = [dict(zip(labels, line)) for line in lines[1:]]
-  print(len(samples))
+
+  for sample in samples:
+    del sample[labels[0]]  # Ignore plant id.
+  labels = labels[1:]
+
+  # Includes the empty string.
+  pericarps = sorted(set([x[PERICARP_LABEL] for x in samples]))
+
+  for sample in samples:
+    if sample[PERICARP_LABEL]:
+      sample[PERICARP_LABEL] = pericarps.index(sample[PERICARP_LABEL])
+
+  result = []
+  for sample in samples:
+    result.append(dict((k, float_or_empty(v)) for k, v in sample.items()))
+  return labels, result
+
+
+def main():
+  labels, samples = parse_data()
+
+  for sample in samples:
+    print(','.join([str(sample[x]) for x in labels]))
+
 
 if __name__ == '__main__':
     main()
