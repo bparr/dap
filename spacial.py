94b419e8a7f02769e22348462fb4600aae6ffd8d
Ben Parr <bparr@bparr.com>
2017-06-23 14:05:08 -0400

    First data for HGT1: (0.032967657756483734, 0.0040000000000000001, 698)

--- spacial.py
+++ spacial.py
@@ -11,22 +11,27 @@ from skbio.stats.distance import mantel
 from scipy.spatial.distance import pdist, squareform
 
 INPUT_FILE_PATH = '2016.csv'
-HGT1_LABEL = merge_data.DataKeys.SF16h_HGT1_120.value
 EASTINGS_LABEL = merge_data.DataKeys.GPS_EASTINGS.value
 NORTHINGS_LABEL = merge_data.DataKeys.GPS_NORTHINGS.value
+
+
+def compute_spacial_correlation(samples, data_key):
+  samples = [x for x in samples if x[data_key.value] != '']
+  eastings = [float(x[EASTINGS_LABEL]) for x in samples]
+  northings= [float(x[EASTINGS_LABEL]) for x in samples]
+  gps = list(zip(eastings, northings))
+  data = [(x[data_key.value], 0.0) for x in samples]
+
+  gps_distances = DistanceMatrix(squareform(pdist(gps)))
+  data_distances = DistanceMatrix(squareform(pdist(data)))
+  return mantel(gps_distances, data_distances)
+
 
 def main():
   labels, *samples = csv_utils.read_csv(INPUT_FILE_PATH)
   samples = [dict(zip(labels, line)) for line in samples]
 
-  height1_samples = [x for x in samples if x[HGT1_LABEL] != '']
-  eastings = [float(x[EASTINGS_LABEL]) for x in height1_samples]
-  northings= [float(x[EASTINGS_LABEL]) for x in height1_samples]
-  gps = list(zip(eastings, northings))
-
-
-  gps_distances = DistanceMatrix(squareform(pdist(gps)))
-  print(gps_distances)
+  print(compute_spacial_correlation(samples, merge_data.DataKeys.SF16h_HGT1_120))
 
 
 if __name__ == '__main__':
