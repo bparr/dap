74b3d8b700f0a5b8a6c4231e536c7eff14880ca3
Ben Parr <bparr@bparr.com>
2017-06-23 14:55:56 -0400

    Also run analysis on merged dataset.

--- spacial.py
+++ spacial.py
@@ -7,13 +7,14 @@ Compute spacial correlations and their significance.
 import csv
 import csv_utils
 import merge_data
+import numpy as np
 import random
 from skbio import DistanceMatrix
 from skbio.stats.distance import mantel
 from scipy.spatial.distance import pdist, squareform
 
-INPUT_FILE_PATH = '2016.csv'
-OUTPUT_FILE_PATH = 'spacial.' + INPUT_FILE_PATH
+INPUT_PATHS = ['2016.csv', '2016.merged.csv']
+OUTPUT_PATHS = ['spacial.' + x for x in INPUT_PATHS]
 EASTINGS_LABEL = merge_data.DataKeys.GPS_EASTINGS.value
 NORTHINGS_LABEL = merge_data.DataKeys.GPS_NORTHINGS.value
 
@@ -23,12 +24,17 @@ MAXIMUM_SIGNIFICANT_P_VALUE = 0.01
 MANTEL_PERMUTATIONS = 10000
 
 
+def average_mismatch(value):
+  return np.mean([float(x) for x in value.split(merge_data.MISMATCH_DELIMETER)])
+
+
 def write_spacial_correlation(csv_writer, samples, data_key):
   samples = [x for x in samples if x[data_key.value] != '']
   eastings = [float(x[EASTINGS_LABEL]) for x in samples]
   northings= [float(x[EASTINGS_LABEL]) for x in samples]
   gps = list(zip(eastings, northings))
-  data = [(x[data_key.value], 0.0) for x in samples]
+  # Add required second dimension, but set to 0.0, so no affect on distances.
+  data = [(average_mismatch(x[data_key.value]), 0.0) for x in samples]
 
   # Sanity check: Random data should show no significant correlation.
   # Results: p-value of 0.905, so sanity check passed.
@@ -49,23 +55,24 @@ def write_spacial_correlation(csv_writer, samples, data_key):
 
 
 def main():
-  labels, *samples = csv_utils.read_csv(INPUT_FILE_PATH)
-  samples = [dict(zip(labels, line)) for line in samples]
+  for input_path, output_path in zip(INPUT_PATHS, OUTPUT_PATHS):
+    labels, *samples = csv_utils.read_csv(input_path)
+    samples = [dict(zip(labels, line)) for line in samples]
 
-  with open(OUTPUT_FILE_PATH, 'w') as f:
-    csv_writer = csv.writer(f)
-    csv_writer.writerow(['label', 'num_data_points', 'corr_coeff', 'p_value'])
+    with open(output_path, 'w') as f:
+      csv_writer = csv.writer(f)
+      csv_writer.writerow(['label', 'num_data_points', 'corr_coeff', 'p_value'])
 
-    for data_key in merge_data.DataKeys:
-      if data_key == merge_data.DataKeys.PLANT_ID:
-        print('Skipping:', data_key.value)
-        continue
-      if data_key == merge_data.DataKeys.ACCESSION_PHOTOPERIOD:
-        # This is a bit hacky way to skip remaining values that are all text
-        # values. But it works nicely right now.
-        break
+      for data_key in merge_data.DataKeys:
+        if data_key == merge_data.DataKeys.PLANT_ID:
+          print('Skipping:', data_key.value)
+          continue
+        if data_key == merge_data.DataKeys.ACCESSION_PHOTOPERIOD:
+          # This is a bit hacky way to skip remaining values that are all text
+          # values. But it works nicely right now.
+          break
 
-      write_spacial_correlation(csv_writer, samples, data_key)
+        write_spacial_correlation(csv_writer, samples, data_key)
 
 
 if __name__ == '__main__':
