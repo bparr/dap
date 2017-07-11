23277fa5439e50a50598f8993f86269710f23222
Ben Parr <bparr@bparr.com>
2017-07-11 13:03:48 -0400

    Untested run_lab_rf.sh script.

--- dataviews/2016/run_lab_rf.sh
+++ dataviews/2016/run_lab_rf.sh
@@ -2,5 +2,5 @@
 
 for file in *.csv
 do
-  ~/sorghum/random_forest/random_forest ds "$file" label ${file%.*} option regression
+  ~/sorghum/random_forest/random_forest ds "$file" label ${file%.*} option regression > stdout.txt && mkdir -p ../../labCodeOutputs/${file%.*} && mv *.csv stdout.txt ../../labCodeOutputs/${file%.*}
 done
