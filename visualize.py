48fe5f53a40c46aa281d8efaad71883b22dcddf6
Ben Parr <bparr@bparr.com>
2017-07-19 16:00:24 -0400

    Add empty visualize.py.

---
+++ visualize.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Visualize specific features of the field.
+"""
+# TODO improve file doc. Including a Usage:.
+# TODO tests?
+
+import csv_utils
+import matplotlib.pyplot as plt
+from merge_data import DataKeys, MISMATCH_DELIMETER
+
+
+def main():
+  print('Hello World')
+
+
+if __name__ == '__main__':
+    main()
